07ed69a95c666006b11556d05cf55bc518554781
cow785785 <zcbm030199@gmail.com>
2023-06-01 00:41:01 +0800

    first commit

---
+++ src/main/java/com/example/Dynamic_questionnaires/DynamicQuestionnairesApplication.java
@@ -0,0 +1,13 @@
+package com.example.Dynamic_questionnaires;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicQuestionnairesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicQuestionnairesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Dynamic_questionnaires/DynamicQuestionnairesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Dynamic_questionnaires;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicQuestionnairesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
